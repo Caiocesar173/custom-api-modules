69291713c5a9fef0f8cbc947e29c051306ddbab5
Caiocesar173 <caiocesar173caiocesar173@gmail.com>
2022-08-16 01:15:08 -0300

    Updating the documentation for the repository; Removing the auto create of the Controller;

--- src/Generators/ModuleGenerator.php
+++ src/Generators/ModuleGenerator.php
@@ -392,13 +392,13 @@ class ModuleGenerator extends Generator
             ]);
         }
 
-        if (GenerateConfigReader::read('controller')->generate() === true) {
-            $options = $this->type=='api'?['--api'=>true]:[];
-            $this->console->call('module:make-controller', [
-                'controller' => $this->getName() . 'Controller',
-                'module' => $this->getName(),
-            ]+$options);
-        }
+        //if (GenerateConfigReader::read('controller')->generate() === true) {
+        //    $options = $this->type=='api'?['--api'=>true]:[];
+        //    $this->console->call('module:make-controller', [
+        //        'controller' => $this->getName() . 'Controller',
+        //        'module' => $this->getName(),
+        //    ]+$options);
+        //}
     }
 
     /**
